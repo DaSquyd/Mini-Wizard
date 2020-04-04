a6a0784f3b535bf6051a522be7aec2a445fe4ebf
Carlos Torres <carlos4torres_12@hotmail.com>
2020-04-04 18:15:56 -0400

    Main Menu
    Added Rough User Interface for Main Menu with Controls

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject howtoplay;
+    public GameObject mainmenu;
+
+    private GameObject disableObject;
+
+    private void Start()
+    {
+        disableObject = FindObjectOfType<GameManager>().gameObject;
+        if (disableObject != null)
+            disableObject.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        if (disableObject != null)
+            disableObject.SetActive(true);
+
+        SceneManager.LoadScene("Ice Cave");
+    }
+
+    public void HowToPlay()
+    {
+        mainmenu.SetActive(false);
+        howtoplay.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainmenu.SetActive(true);
+        howtoplay.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
