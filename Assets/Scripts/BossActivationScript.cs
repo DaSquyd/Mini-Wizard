57cb7376b05751e70dd74bee58aed9ac7d27cbe1
Carlos Torres <carlos4torres_12@hotmail.com>
2020-04-20 18:14:00 -0400

    Boss Fully Implemented!
    Correct model added, and all mechanics fully functional!

--- Assets/Scripts/BossActivationScript.cs
+++ Assets/Scripts/BossActivationScript.cs
@@ -6,12 +6,15 @@ public class BossActivationScript : MonoBehaviour
 {
 
     public BossScript bossScript;
+    public GameObject healthBar;
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.tag);
         if (other.tag == "Player")
         {
             bossScript.enabled = true;
+            healthBar.SetActive(true);
         }
     }
 
--- Assets/Scripts/Game/BossScript.cs
+++ Assets/Scripts/Game/BossScript.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossScript : Entity
 {
+
+    public Slider healthBar;
 
     [Header("Movement")]
     public float movementSpeed = 2.0f;
 
     [Header("Slamming")]
-    public GameObject punchBox;
     public float slamSeconds = 4.0f;
     public float slamRate = 0.5f;
 
     [Header("Throwing")]
+    public GameObject throwPlace;
+    public GameObject throwItem;
     public float throwSeconds = 5.0f;
     public float throwRate = 0.5f;
+    public float throwForce = 5.0f;
 
     private enum EnemyState
     {
@@ -32,11 +37,21 @@ public class BossScript : Entity
     private float nextSlam;
     [DebugDisplay]
     private float disToPlayer;
+    private Animator animator;
+    [DebugDisplay]
+    private bool once;
+    private bool punched;
 
     protected override void Start()
     {
         base.Start();
         enemyState = EnemyState.Idle;
+        animator = GetComponent<Animator>();
+        nextSlam = Time.time + slamSeconds + 1.0f / slamRate;
+        nextThrow = Time.time + throwSeconds + 1.0f / throwRate;
+        MaxHealth = 300;
+        Health = 300;
+        healthBar.maxValue = MaxHealth;
     }
 
     protected override void Update()
@@ -46,8 +61,13 @@ public class BossScript : Entity
         if (player == null && FindObjectOfType<PlayerController>() != null)
             player = FindObjectOfType<PlayerController>().gameObject;
 
-        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Vector3.up);
+        if (player != null)
+        {
+            transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Vector3.up);
+        }
         disToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        animator.SetBool("Walking", enemyState == EnemyState.Idle);
 
         if (player != null)
         {
@@ -55,42 +75,62 @@ public class BossScript : Entity
             {
                 case EnemyState.Idle:
                     //rotate towards 
-                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Time.deltaTime * movementSpeed);
-                    if (Vector3.Distance(transform.position, player.transform.position) >= 12.5f && Time.time > nextThrow)
+                    if (Vector3.Distance(transform.position, player.transform.position) > 7.0f)
+                        transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Time.deltaTime * movementSpeed);
+                    if (Vector3.Distance(transform.position, player.transform.position) >= 15.0f && Time.time > nextThrow)
                         enemyState = EnemyState.Throw;
-                    if (Vector3.Distance(transform.position, player.transform.position) < 5.0f && Time.time > nextSlam)
+                    if (Vector3.Distance(transform.position, player.transform.position) < 10.0f && Time.time > nextSlam)
                         enemyState = EnemyState.Slam;
                     break;
                 case EnemyState.Slam:
                     //Punch the player
                     //do the things
+                    animator.SetTrigger("Slam");
                     nextSlam = Time.time + slamSeconds + 1.0f / slamRate;
                     StartCoroutine(Slam());
                     break;
                 case EnemyState.Throw:
                     //Throw a big rock
+                    if (!once)
+                        ThrowRock();
                     nextThrow = Time.time + throwSeconds + 1.0f / throwRate;
                     StartCoroutine(Throw());
                     break;
                 case EnemyState.Die:
                     //Play death animation
+                    animator.SetBool("Death", true);
+                    enabled = false;
                     break;
             }
         }
+
+        healthBar.value = Health;
     }
 
     private IEnumerator Slam()
     {
         yield return new WaitForSecondsRealtime(slamSeconds);
-
         enemyState = EnemyState.Idle;
     }
 
     private IEnumerator Throw()
     {
         yield return new WaitForSecondsRealtime(throwSeconds);
-
+        once = false;
         enemyState = EnemyState.Idle;
+    }
+
+    private void ThrowRock()
+    {
+        animator.SetTrigger("Throw");
+        GameObject throwObject = Instantiate(throwItem, throwPlace.transform.position, Quaternion.identity);
+        throwObject.transform.localScale = new Vector3(0.4f, 0.5f, 1.0f);
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        throwObject.AddComponent<BoxCollider>();
+        Rigidbody throwRB = throwObject.AddComponent<Rigidbody>();
+        throwRB.AddForce(direction * throwForce, ForceMode.Force);
+        Destroy(throwObject, 5.0f);
+        once = true;
     }
 
 }
