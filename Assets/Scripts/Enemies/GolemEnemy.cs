df33ad3dd09cc4e98e73dd1b011072fa8b72c0e2
David Jacquish <david.jacquish@septyr.com>
2020-04-21 00:44:28 -0400

    Lil guys

---
+++ Assets/Scripts/Enemies/GolemEnemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GolemEnemy : Entity
+{
+	public GolemSettings Settings;
+
+	public bool IsTargeting
+	{
+		get; private set;
+	}
+
+	enum State
+	{
+		Idle,
+		Aggressive
+	}
+	State state = State.Idle;
+
+	Vector3 origin;
+
+	NavMeshAgent agent;
+
+	protected override void Start()
+	{
+		base.Start();
+
+		MaxHealth = Settings.MaxHealth;
+		Health = MaxHealth;
+
+		origin = transform.position;
+
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+
+		RaycastHit[] coneHit = ConeCast.ConeCastAll(transform.position, transform.forward, Settings.SightMaxDistance, Settings.SightAngle, LayerMask.GetMask("Default"));
+
+		IsTargeting = false;
+		for (int i = 0; i < coneHit.Length; i++)
+		{
+			if (coneHit[i].collider.tag == "Player")
+			{
+				IsTargeting = true;
+				break;
+			}
+		}
+
+		if (PlayerController.Instance != null && Vector3.Distance(transform.position, PlayerController.Instance.transform.position) <= Settings.SensingRadius)
+			IsTargeting = true;
+	}
+
+	private void FixedUpdate()
+	{
+		if (IsTargeting)
+			agent.SetDestination(PlayerController.Instance.transform.position);
+	}
+}
---
+++ Assets/Scripts/Enemies/GolemSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Fire and Ice/Enemy/Golem Settings")]
+public class GolemSettings : ScriptableObject
+{
+	public int MaxHealth = 24;
+
+	[Header("Sensing")]
+	public float SightMaxDistance = 16f;
+	public float SightAngle = 170f;
+	public float SensingRadius = 5f;
+}
